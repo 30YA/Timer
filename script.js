3410ada7ec6cbb4414576cb75a30a5c23de01006
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-02 22:56:27 +0330

    version 2.0 (fix a lot of bugs)

--- script.js
+++ script.js
@@ -7,20 +7,36 @@ const secend = document.querySelector(".titles .box .secend");
 const startBtn = document.querySelector(".start");
 const getnumBtn = document.querySelector(".getnum");
 let setinter;
-
-const startinghour = 60;
-let time = startinghour * 60;
-console.log(time);
+let clicker = 0;
 
 startBtn.addEventListener('click', () => {
-    setinter = setInterval(() => {
-        if (time == 0) {
-            clearInterval(setinter);
-        }
-        let minutes = Math.floor(time / 60);
-        let secends = time % 60;
-        time--;
-        minute.innerHTML = `${minutes}`;
-        secend.innerHTML = `${secends}`;
-    },10);
+    let time = startinghour * 60;
+    clicker++;
+    if (clicker == 1) {
+        setinter = setInterval(() => {
+            if (time == 0) {
+                clearInterval(setinter);
+                clicker = 0;
+            }
+            let minutes = Math.floor(time / 60);
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            let secends = time % 60;
+            secends = secends < 10 ? "0" + secends : secends;
+            time--;
+            minute.innerHTML = `${minutes}`;
+            secend.innerHTML = `${secends}`;
+        },1000);
+    }else{
+        clearInterval(setinter);
+        clicker = 0;
+    }
+})
+let today = (new Date()).getTime();
+console.log(today);
+
+let startinghour = 0;
+getnumBtn.addEventListener('click', () => {
+    startinghour = +prompt("Enter the minute");
+    minute.innerHTML = startinghour < 10 ? "0" + startinghour : startinghour;
+    secend.innerHTML = "0" + 0;
 })
